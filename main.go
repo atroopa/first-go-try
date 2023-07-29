dff66597542dbfb2c2c2f2b6667c02c2b216d435
atroopa <atroopa@gmail.com>
2023-07-29 18:41:44 +0330

    clean eextra

--- main.go
+++ main.go
@@ -3,25 +3,13 @@ package main
 
 import (
 	"fmt"
-	"time"
-
-	Jalaali "github.com/jalaali/go-jalaali"
 )
 
 func main (){
 
 	// variables
-	var annualSalary string
-	var bigNumber int
-
-	annualSalary = "golnaz"
+	var annualSalary string = "golnaz"
 	sayHello(annualSalary)
-
-	bigNumber = 12
-	fmt.Println(bigNumber)
-
-	fmt.Println(Jalaali.ToJalaali(time.Now().Date()))
-
 }
 
 
